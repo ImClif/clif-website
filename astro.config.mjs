6f02afa3b779a1570a00ab3dd9cd305020d51a70
Bryce Russell <brycetrussell@gmail.com>
2024-11-29 16:46:43 -0600

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), tailwind()]
+});
